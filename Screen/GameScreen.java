4368cd353354f06253d8f5457d9f11c3cbe4c2d8
HieuNguyen <nguyenminhhieuclls@gmail.com>
2025-02-21 23:18:08 +0700

    update

--- Screen/GameScreen.java
+++ Screen/GameScreen.java
@@ -1,17 +1,17 @@
 package Screen;
+
 import javax.swing.JFrame;
 
-public class GameScreen extends JFrame  {
+public class GameScreen extends JFrame {
 
-    private static final int originalTileSize =  16; //16x16
+    private static final int originalTileSize = 16; // 16x16
     private static final int scale = 3;
-    public static final int tileSize = originalTileSize * scale;//48x48
+    public static final int tileSize = originalTileSize * scale;// 48x48
     private static final int maxScreenCol = 16;
     private static final int maxScreenRow = 12;
-    public static final int with = tileSize * maxScreenCol;//768
-    public static final int height = tileSize * maxScreenRow;//576
+    public static final int with = tileSize * maxScreenCol;// 768
+    public static final int height = tileSize * maxScreenRow;// 576
     private GamePanle backGround = new GamePanle();
-
 
     public GameScreen() {
         this.add(backGround);
@@ -19,12 +19,12 @@ public class GameScreen extends JFrame  {
         setSize(with, height);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
-        setResizable(false);
+        setResizable(true);
         this.setVisible(true);
     }
+
     public void startGame() {
         backGround.startGame();
     }
 
-    
 }
