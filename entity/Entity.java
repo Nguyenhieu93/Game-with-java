27c8b127c4c94c702b639b1bc5d80ce493a36e41
Manhddm <manhducdongyb@gmail.com>
2025-02-22 07:47:09 +0700

    Add getter setter cho viec draw

--- entity/Entity.java
+++ entity/Entity.java
@@ -1,9 +1,24 @@
 package entity;
 
+import java.awt.image.BufferedImage;
 public class Entity {
     private int x;
     private int y;
     private int speed ;
+    private int frameWith;
+    private int frameHeight;
+    private int totalFrames;
+    private int currentFrame;
+    private int frameDelay;
+    private int frameCounter;
+
+    private BufferedImage image;
+    public void setImage(BufferedImage image){
+        this.image = image;
+    }
+    public BufferedImage getImage(){
+        return image;
+    }
     public void setX(int x){
         this.x = x;
     }
@@ -21,5 +36,41 @@ public class Entity {
     }
     public int getSpeed(){
         return speed;
+    }
+    public void setFrameWith(int with){
+        this.frameWith = with;
+    }
+    public int getFrameWith(){
+        return this.frameWith;
+    }
+    public void setFrameHeight(int height){
+        this.frameHeight = height;
+    }
+    public int getFrameHeight(){
+        return this.frameHeight;
+    }
+    public void setTotalFrame(int total){
+        this.totalFrames = total;
+    }
+    public int getTotalFrame(){
+        return this.totalFrames;
+    }
+    public void setCurrentFrame(int current){
+        this.currentFrame = current;
+    }
+    public int getCurrenFame(){
+        return this.currentFrame;
+    }
+    public void setFrameDelay(int delay){
+        this.frameDelay = delay;
+    }
+    public int getFrameDelay(){
+        return this.frameDelay;
+    }
+    public void setFrameCounter(int counter){
+        this.frameCounter = counter;
+    }
+    public int getFrameCounter(){
+        return this.frameCounter;
     }
 }
